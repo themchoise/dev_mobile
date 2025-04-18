6b8d4bb39ad65ee06017cb9a2700a2d659921e58
Maximiliano Joyce Sp Team <maximilianojoyce@gmail.com>
2025-04-18 13:27:13 -0300

    feat: add currency

--- app/src/main/java/com/example/mjoyce/tp1/ej2_conversor/ConversorView.kt
+++ app/src/main/java/com/example/mjoyce/tp1/ej2_conversor/ConversorView.kt
@@ -2,20 +2,31 @@ package com.example.mjoyce.tp1.ej2_conversor
 
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.view.Gravity
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mjoyce.R
-import com.example.mjoyce.tp1.ej1_calculadora.Calculadora
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import android.text.TextWatcher
+import android.text.Editable
+import android.view.View
+import android.widget.Spinner
 
 class ConversorView : AppCompatActivity() {
+
+    private lateinit var currency: CurrencyResponse
+
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +38,79 @@ class ConversorView : AppCompatActivity() {
             insets
         }
 
-        val apiStart = ApiCurrency()
-        var currency
+        val spinner: Spinner = findViewById(R.id.spnLoad)
         CoroutineScope(Dispatchers.Main).launch {
+            val apiStart = ApiCurrency()
+            var currSelected: String = ""
+
             currency = withContext(Dispatchers.IO) {
                 apiStart.getCurrencyes()
             }
+
+            val valueToConvert: EditText = findViewById(R.id.editTextText3)
+            val valueResult: TextView = findViewById(R.id.textResult)
+
+
+            val buttons: List<RadioButton> = listOf(
+                findViewById(R.id.rbusd),
+                findViewById(R.id.rbeur),
+                findViewById(R.id.rbbrl),
+            )
+
+            buttons.forEach { button ->
+                button.setOnClickListener {
+                    if (valueToConvert.text.toString().isEmpty()) {
+                        val toast = Toast.makeText(
+                            this@ConversorView,
+                            "Debe ingresar un valor",
+                            Toast.LENGTH_SHORT
+                        )
+                        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 200)
+                        toast.show()
+
+                    } else {
+
+                        val currencySelected: String = button.text.toString()
+                        currSelected = currencySelected
+
+                        buttons.filter { it.text.toString() != currencySelected }.forEach {
+                            it.isChecked = false
+                        }
+                        spinner.isEnabled = true
+                        spinner.visibility = View.VISIBLE
+                        val result = valueToConvert.text.toString()
+                            .toFloat() * currency.rates[currencySelected].toString().toFloat()
+                        valueResult.text = "$currencySelected: $result"
+                    }
+
+
+                }
+
+            }
+            valueToConvert.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (currSelected.isNotEmpty()) {
+                        val nuevoValor = s.toString()
+                        val result = nuevoValor
+                            .toFloat() * currency.rates[currSelected].toString().toFloat()
+                        valueResult.text = "$currSelected: $result"
+                    }
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
+
         }
-
-
-        val btnUSD: Button = findViewById(R.id.rbusd)
-        val btnEUR: Button = findViewById(R.id.rbeur)
-        val btnBRL: Button = findViewById(R.id.rbbrl)
-
-
     }
 }
